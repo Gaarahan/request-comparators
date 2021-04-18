0c41c0f8b17aa532e1ad0238ddb2460958556217
Gaarahan <gaarahan@foxmail.com>
2021-04-18 23:03:00 +0800

    [feature] add time as suffix

--- components/param-list/param-item/ParamItem.js
+++ components/param-list/param-item/ParamItem.js
@@ -6,9 +6,20 @@ export default class ParamItem extends ElementCreator {
     super();
     // TODO 添加全部展开与收起按钮
     // this.curPrefix = new ParamPrefix().getElement();
-    this.element = document.createElement('li');
-    JsonView.renderJSON(param, this.element);
+
+    this.renderElement(param);
+
     // this.element.insertAdjacentElement('afterbegin', this.curPrefix)
     // this.element.insertAdjacentText('beforeend', `${JSON.stringify(param)}`)
+  }
+
+  renderElement(param) {
+    this.element = document.createElement('li');
+    JsonView.renderJSON(param, this.element);
+
+    const suffixEle = document.createElement('span');
+    suffixEle.innerText = new Date().toTimeString();
+
+    this.element.insertAdjacentElement('beforeend', suffixEle);
   }
 }
